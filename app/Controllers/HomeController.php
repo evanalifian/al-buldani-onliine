9ef1ced164302e9e80fb2ec585ddb9c5b925eefe
evanalifian <evanalifian@gmal.com>
2025-08-09 01:18:41 +0700

    create blog model

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -3,12 +3,15 @@
 namespace AlBuldaniOnline\Controllers;
 
 use AlBuldaniOnline\Core\View;
+use AlBuldaniOnline\Models\BlogModel;
 
 class HomeController {
   public function index(): void {
     View::render("home/index", [
       "title" => "Al-Buldani Online",
-      "content" => "Website portal berita Pondok Pesantren Al-Buldani Pamekasan."
+      "top_blog" => BlogModel::getTopBlog(),
+      "top_two_blog" => BlogModel::getSomeBlogs(2),
+      "recent_posts" => BlogModel::getSomeBlogs(3),
     ]);
   }
 }

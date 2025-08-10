256799ea6c26296302e2e28abb8fa583b7fba81c
evanalifian <evanalifian@gmal.com>
2025-08-10 23:53:01 +0700

    creat blog post route

--- public/index.php
+++ public/index.php
@@ -6,8 +6,12 @@ use AlBuldaniOnline\Controllers\BlogController;
 use AlBuldaniOnline\Core\Router;
 use AlBuldaniOnline\Controllers\HomeController;
 
+// Home Route
 Router::add("GET", "/", HomeController::class, "index");
+
+// Blog Route
 Router::add("GET", "/blogs", BlogController::class, "index");
 Router::add("GET", "/blogs/([0-9a-zA-Z-]*)", BlogController::class, "category");
+Router::add("GET", "/blogs/([0-9a-zA-Z-]*)/([0-9a-zA-Z-]*)", BlogController::class, "blog");
 
 Router::run();

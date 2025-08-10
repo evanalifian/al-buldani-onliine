22a8f02b0d6d8a3204d02dcab04e49a0d223b229
evanalifian <evanalifian@gmal.com>
2025-08-10 23:49:21 +0700

    create conditional category and add method blog post

--- app/Controllers/BlogController.php
+++ app/Controllers/BlogController.php
@@ -2,6 +2,7 @@
 
 namespace AlBuldaniOnline\Controllers;
 
+use AlBuldaniOnline\Core\Utils;
 use AlBuldaniOnline\Core\View;
 
 class BlogController {
@@ -10,9 +11,22 @@ class BlogController {
   }
 
   public static function category(string $category): void {
-    View::render("blog/category", [
-      "category" => $category
+    $cond = strtolower(Utils::parseTitle($category)) == "kabar pesantren" || strtolower(Utils::parseTitle($category)) == "kajian islami" || strtolower(Utils::parseTitle($category)) == "cerita islami";
+    if ($cond) {
+      View::render("blog/category", [
+        "category" => Utils::parseTitle($category)
+      ]);
+    } else {
+      http_response_code(404);
+      require __DIR__ . "/../Views/404.php";
+    }
+    
+  }
+
+  public static function blog(string $category, string $blogTitle): void {
+    View::render("blog/blog", [
+      "category" => Utils::parseTitle($category),
+      "blogTitle" => Utils::parseTitle($blogTitle)
     ]);
-    // echo $category;
   }
 }
